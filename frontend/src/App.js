12ac333b92de3203abd96fabbf2f1398856a0b84
Ritesh Gupta <65335183+Ritesh9793@users.noreply.github.com>
2025-02-22 12:07:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import AddRecipe from "./pages/AddRecipe";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
